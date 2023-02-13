966f9f93f6b2cede47964d2eb9a0d451e07d5e4a
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-02-13 16:01:06 +0300

    Completed assignment

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,42 @@
+require  'pry'
+class CashRegister
 
+    attr_accessor :total, :pay_slip
+    attr_reader :discount
+  
+    def initialize(discount = 0)
+      @total = 0
+      @discount = discount
+      @pay_slip = []
+    end
+  #binding.pry
+    def add_item(title, price, quantity = 1)
+      @total += price * quantity
+      quantity.times { @pay_slip.push({ title: title, price: price, quantity: quantity }) }
+    end
+  #binding.pry
+    def apply_discount
+
+      if @discount > 0
+        amount_saved = @total * @discount / 100
+        @total -= amount_saved
+          "After the discount, the total comes to $#{@total}."
+      else
+          "There is no discount to apply."
+      end
+
+    end
+  #binding.pry
+    def items
+      @pay_slip.map { |item| item[:title] }
+    end
+  #binding.pry
+    def void_last_transaction
+      final_transaction = pay_slip.pop
+      #binding.pry
+      @total -= final_transaction[:price] * final_transaction[:quantity]
+    end
+    #binding.pry
+end
+#binding.pry
+CashRegister.new(20)
